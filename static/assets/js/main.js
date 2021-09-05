95f2f6a56edf8499e53c483e37009b83fa3387b9
j7k6 <mail@j7k6.org>
2021-09-05 13:10:16 +0200

    updated javascript

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,207 +1,157 @@
+'use strict';
+
+const itemTypes = { 'group': 'group', 'feed': 'feed' };
+const docTitle = document.title;
+
 var lastTimestamp = 0;
 var showFeeds = false;
-var firstRun = true;
-var newItemsNum = 0;
-var docTitle = document.title;
-
+var newItemsCount = 0;
+var itemType = 'all';
+var itemTypeId = '';
 var groups, feeds;
-var groupId = ''
-var feedId = '';
 
-
-function hashChange() {
-  if (window.location.hash) {
-    var hash = window.location.hash.substring(1);
-    var params = hash.split('/');
-
-    if (params[1] === 'all') {
-      groupId, feedId = '';
-
-      Array.from(document.querySelectorAll('.active')).forEach((el) => el.classList.remove('active'));
-
-      document.querySelector('.group.all h3').classList.add('active');
-
-      setToggleNav('group', 'all feeds');
-    } else {
-      if (params.length === 3) {
-        if (params[1] == 'group') {
-          groupId = params[2];
-        }
-
-        if (params[1] == 'feed') {
-          feedId = params[2];
-        }
-      }
-    }
-  } else {
-    groupId, feedId = ''
-  }
-
-  firstRun = true;
-
-  toggleFeeds();
-
-  window.scrollTo(0, 0);
-
-  getItems(0);
-} 
 
 function toggleFeeds() {
-  document.title = docTitle;
-  document.querySelector('.counter').innerText = '';
-
-  var feedsBlock = document.querySelector('.feeds');
+  let feedsBlock = document.querySelector('.feeds');
 
   if (showFeeds) {
     feedsBlock.classList.remove('show');
+
     showFeeds = false;
   } else {
-    window.scrollTo(0, 0);
-
     feedsBlock.classList.add('show');
+
     showFeeds = true;
+  }
+
+  window.scrollTo(0, 0);
+}
+
+
+function setToggleNavInfo(itemType, itemTypeId) {
+  let info = document.querySelector('.toggle span.info');
+
+  info.className = 'info';
+
+  switch (itemType) {
+    case 'all':
+      info.innerHTML = 'all feeds';
+      break;
+
+    case 'group':
+      info.innerHTML = `${itemTypeId}`;
+      break;
+
+    case 'feed':
+      info.innerHTML = `${feeds.find(feed => feed.id === itemTypeId).title}`;
+      info.classList.add(`feed_${itemTypeId}`);
+      break;
   }
 }
 
 
-function setToggleNav(type, id, title='') {
-  var groupInfo = document.querySelector('.toggle span.group');
-  var feedInfo = document.querySelector('.toggle span.feed');
+function setNewItemsCounter(newItems) {
+  newItemsCount += newItems;
 
-  groupInfo.innerHTML = '';
-  feedInfo.innerHTML = '';
-  feedInfo.className = 'feed'
+  let counterValue = newItemsCount;
 
-  if (type === 'group') {
-    groupInfo.innerHTML = id;
-  }
+  if (newItemsCount > 99)
+    counterValue = '99+';
 
-  if (type === 'feed') {
-    feedInfo.innerHTML = title;
-    feedInfo.classList.add(`feed_${id}`)
-  }
+  document.querySelector('.counter').innerText = counterValue;
+  document.title = `${docTitle} (${counterValue})`;
+}
+
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false
+  }).format(new Date(date*1000));
 }
 
 
 function getItems(since) {
-  var fetchUrl = `/api/getItems?since=${since}`;
+  let fetchUrl = `/api/getItems?since=${since}`;
 
-  if (feedId !== '') {
-    groupId = '';
-    fetchUrl = `${fetchUrl}&feed_id=${feedId}`;
-  }
-
-  if (groupId !== '') {
-    feedId = '';
-    fetchUrl = `${fetchUrl}&group_id=${groupId}`;
-  }
-
-  if (firstRun) {
-    document.querySelector('.items').innerHTML = '';
-  } else {
-    Array.from(document.querySelectorAll('article')).forEach((el) => el.classList.remove('new'));
-  }
+  if (itemType !== 'all')
+    fetchUrl = `${fetchUrl}&${itemTypes[itemType]}_id=${itemTypeId}`;
 
   fetch(fetchUrl).then(function(res) {
     return res.json();
   }).then(function(data) {
-    if (data.length > 0) {
-      console.log(data.length);
-      lastTimestamp = data[0].added;
+    let items = data;
+    let lastScrollPos = window.scrollY;
+    let itemsHtml = document.querySelector('.items');
+    let itemsHtmlStyle = window.getComputedStyle(itemsHtml);
+    let itemsHtmlHeightBefore = itemsHtml.offsetHeight + parseInt(itemsHtmlStyle.marginTop) + parseInt(itemsHtmlStyle.marginBottom);
 
-      if (!firstRun) {
-        var counterValue;
+    if (since === 0)
+      itemsHtml.innerHTML = '';
 
-        newItemsNum += data.length;
+    if (items.length > 0) {
+      console.log(items.length);
+      lastTimestamp = items[0].added;
 
-        if (newItemsNum > 99) {
-          counterValue = '99+';
-        } else {
-          counterValue = newItemsNum;
-        }
+      if (since > 0)
+        setNewItemsCounter(items.length);
 
-        document.querySelector('.counter').innerText = counterValue;
-        document.title = `${docTitle} (${counterValue})`;
+      Array.from(document.querySelectorAll('article')).forEach((el) => el.classList.remove('new'));
+
+      itemsHtml.innerHTML =
+        `${items.map(item =>
+          `<article class="new">
+             <h5 class="feed_${item.feed}">${feeds.find(feed => feed.id === item.feed).title}:</h5>
+             <h4><a href="${item.link}" target="_blank">${item.title}</a></h4>
+             <h6>${formatDate(item.published)}</h6>
+             <p>${item.description}</p>
+          </article>`
+        ).join('')}` + itemsHtml.innerHTML;
+
+      if (since > 0 && window.scrollY > 0) {
+        let itemsHtmlHeightAfter = itemsHtml.offsetHeight + parseInt(itemsHtmlStyle.marginTop) + parseInt(itemsHtmlStyle.marginBottom);
+        let itemsHtmlHeightDiff = itemsHtmlHeightAfter - itemsHtmlHeightBefore;
+
+        window.scrollTo(0, (lastScrollPos + itemsHtmlHeightDiff));
       }
-
-      data.reverse().forEach(function(item) {
-        var feed = feeds.find(f => f.id === item.feed);
-
-        var published = new Intl.DateTimeFormat('en-US', {
-          year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false
-        }).format(new Date(item.published*1000));
-
-        var lastScrollPos = window.scrollY;
-
-        var newItem = document.createElement('article');
-
-        newItem.innerHTML = `
-                <h5 class="feed_${feed.id}">${feed.title}:</h5>\
-                <h4><a href="${item.link}" target="_blank">${item.title}</a></h4>\
-                <h6>${published}</h6>\
-                <p>${item.description}</p>`;
-
-        newItem.classList.add('new');
-
-        document.querySelector('.items').prepend(newItem);
-
-        if (!firstRun && window.scrollY > 0) {
-          var prependItem = document.querySelector('.items article:first-child');
-          var prependItemStyle = window.getComputedStyle(prependItem);
-          var prependItemHeight = prependItem.offsetHeight + parseInt(prependItemStyle.marginTop) + parseInt(prependItemStyle.marginBottom);
-
-          window.scrollTo(0, (lastScrollPos + prependItemHeight));
-        }
-      });
-
-      firstRun = false;
     }
   }).catch(function(err) {
     console.log(err);
   });
 }
 
-function getFeeds() { 
-  fetch('/api/getFeeds').then(function(res) {
-    return res.json();
-  }).then(function(data) {
-    feeds = data;
 
-    var style = document.createElement('style');
-    style.type = 'text/css';
+function showItems(newItemType, newItemTypeId) {
+  let clickElement = document.querySelector('.group.all')
 
-    feeds.forEach(function(feed) {
-      if (feed.favicon === '') {
-        style.innerHTML = style.innerHTML + `.feed_${feed.id} { padding-left: 0 !important; }`;
-      } else {
-        style.innerHTML = style.innerHTML + `.feed_${feed.id} { background-image: url('data:image/png;base64, ${feed.favicon}'); }`;
-      }
+  itemType = newItemType;
+  itemTypeId = newItemTypeId;
 
-      var newFeed = document.createElement('li');
+  if (itemType !== 'all')
+    clickElement = document.querySelector(`.${itemType}.${itemType}_${itemTypeId}`)
+  
+  Array.from(document.querySelectorAll('.active')).forEach((el) => el.classList.remove('active'));
 
-      newFeed.innerHTML = `<a href="#/feed/${feed.id}">${feed.title}</a>`;
+  clickElement.classList.add('active');
 
-      newFeed.classList.add(`feed_${feed.id}`);
+  setToggleNavInfo(itemType, itemTypeId);
 
-      newFeed.addEventListener('click', function() {
-        Array.from(document.querySelectorAll('.active')).forEach((el) => el.classList.remove('active'));
-        this.classList.add('active');
-        setToggleNav('feed', feed.id, feed.title);
-      });
+  document.querySelector('.feeds').classList.remove('show');
 
-      document.querySelector(`.group.group_${feed.group_id} ul`).append(newFeed);
-    });
+  window.scrollTo(0, 0);
 
-    document.getElementsByTagName('head')[0].appendChild(style);
+  newItemsCount = 0;
 
-    if (window.location.hash === '') {
-      window.location.hash = '#/all';
-    } else {
-      getItems(0);
-    }
-  }).catch(function(err) {
-    console.log(err)
-  });
+  getItems(0);
+}
+
+
+if (window.location.hash) {
+  let params = window.location.hash.substring(1).split('/');
+  
+  itemType = params[1] || 'all';
+  itemTypeId = params[2] || '';
+} else {
+  window.location.hash = '#/all';
 }
 
 fetch('/api/getGroups').then(function(res) {
@@ -209,39 +159,46 @@ fetch('/api/getGroups').then(function(res) {
 }).then(function(data) {
   groups = data;
 
-  groups.forEach(function(groupId) {
-    var newGroup = document.createElement('div');
+  fetch('/api/getFeeds').then(function(res) {
+    return res.json();
+  }).then(function(data) {
+    feeds = data;
 
-    newGroup.innerHTML = `
-        <h3><a href="#/group/${groupId}">${groupId}</a></h3>\
-        <ul></ul>`;
+    document.querySelector('head').innerHTML +=
+      `<style type="text/css">
+        ${feeds.filter(feed => feed.favicon !== '').map(feed =>
+          `.feed_${feed.id} { padding-left: 20px; background-image: url('data:image/png;base64, ${feed.favicon}'); }`
+        ).join('')}
+      </style>`;
 
-    newGroup.classList.add('group', `group_${groupId}`);
+    document.querySelector('.feeds').innerHTML += 
+      `${groups.map(groupId =>
+        `<div class="group group_${groupId}">
+          <h3 onclick="showItems('group', '${groupId}');"><a href="#/group/${groupId}">${groupId}</a></h3>
+          <ul>
+          ${feeds.filter(feed => feed.group_id === groupId).map(feed =>
+            `<li class="feed feed_${feed.id}" onclick="showItems('feed', '${feed.id}');"><a href="#/feed/${feed.id}">${feed.title}</li>`     
+          ).join('')}
+          </ul>
+        </div>`
+      ).join('')}`;
 
-    newGroup.querySelector('h3').addEventListener('click', function() {
-      Array.from(document.querySelectorAll('.active')).forEach((el) => el.classList.remove('active'));
-      this.classList.add('active');
-      setToggleNav('group', groupId);
-    });
-
-    document.querySelector('.feeds').append(newGroup);
+    showItems(itemType, itemTypeId);
+  }).catch(function(err) {
+    console.log(err)
   });
-
-  getFeeds();
 }).catch(function(err) {
   console.log(err)
 });
-
 
 setInterval(function() { 
   getItems(lastTimestamp); 
 }, 5000);
 
-window.addEventListener('hashchange', hashChange, false);
-
 window.addEventListener('scroll', function() {
   if (window.scrollY === 0) {
-    newItemsNum = 0;
+    newItemsCount = 0;
+
     document.querySelector('.counter').innerText = '';
     document.title = docTitle;
   }
